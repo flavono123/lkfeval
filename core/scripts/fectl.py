8673a92953e61fcb778e8636576169ac74d41e34
bigs <wrlawodms@gmail.com>
2016-10-16 19:35:45 +0900

    complete fectl.py
    add command (del, show, list)
    bugfix in add command : json formatting('config')

--- core/scripts/fectl.py
+++ core/scripts/fectl.py
@@ -24,22 +24,21 @@ def shell_command(command, error_str='', print_on_error=True, exit_on_error=True
             exit(1)
     return ret  
 
-def add_feature(features, argv):
-	fname = argv[2]
+def add_feature(features, fname):
 	eval_script = input('feature evalustaion script : ') #TODO check if it exists
 	img_cnt = int(input('the number of kernel images : ')) 
 	imgs = []
 	for i in range(img_cnt) :
 		img = dict()
-		img['version'] = input('image[%d] : kerenl version : ' % i)
+		img['version'] = input('image[%d] - kerenl version : ' % i)
 		img['config'] = dict()
 		print('image[%d] : config info (ctrl+D to finish)' % i)
 		j=0
 		while True :
 			try:
-				config_name = input('image [%d] : kernel config[%d] name : ' % (i, j)) 
-				config_value = input('image [%d] : kernel config[%d] value : ' % (i, j)) 
-				img[config_name] = config_value
+				config_name = input('image [%d] - kernel config[%d] name : ' % (i, j)) 
+				config_value = input('image [%d] - kernel config[%d] value : ' % (i, j)) 
+				img['config'][config_name] = config_value
 			except EOFError:
 				print('')
 				break
@@ -52,20 +51,36 @@ def add_feature(features, argv):
 	features[fname] = feature
 				
 
-def del_featrue(features, argv):
-	pass
+def del_feature(features, fname):
+	del features[fname]
 
-def show_feature(features, argv):
-	pass
+def show_feature(features, fname):
+	try:
+		feature = features[fname]
+	except KeyError:
+		eprint('feature not found.')
+		exit(1)
+	print('feature name : ', fname)
+	print('\tfeature evaluation script : ', feature['evaluation_script'])
+	print('\tthe count of kernel images : ', feature['image_cnt'])
+	feature_imgs = feature['images']
+	for i in range(feature['image_cnt']):
+		print('\timage[%d] - versoin : %s' % (i, feature_imgs[i]['version']))
+		print('\timage[%d] - configs : ' % i)
+		f_configs = feature_imgs[i]['config']
+		for k,v in f_configs.items():
+			print('\t '+k+'='+str(v))
 
-def list_feature(features, argv):
-	opts = ['--detail', '--default']
-	opt = '--default'
-	if len(argv) > 2:
-		opt = argv[2]
+def list_feature(features, opt = '--default'):
+	opts = ['--details', '--default']
 	if opt not in opts:
 		raise Exception('invalid option')
-	
+	if opt == '--default':
+		for fname in features.keys():
+			print(fname)
+	elif opt == '--details':
+		for fname in features.keys():
+			show_feature(features, fname)
 
 if len(sys.argv) < 2 :
 	eprint("missing required arguments")
@@ -91,8 +106,13 @@ else:
 	features = json.loads('{}')
 
 command_handler = getattr(current_module, command+'_feature')
-command_handler(features, sys.argv)
+if len(sys.argv) > 2:
+	command_handler(features, sys.argv[2])
+else:
+	command_handler(features)
 
 feconf_file = open(feconf_file_name, 'w+')
 json.dump(features, feconf_file)
 feconf_file.close()
+
+print('done')
