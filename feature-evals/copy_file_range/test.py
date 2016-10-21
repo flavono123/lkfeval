3038dfd315da38be34c4ba6606bf3cf205415228
Hansuk Hong <flavono123@gmail.com>
2016-10-21 14:07:34 +0900

    update: copy_file_range; adjust fadvise, set buffer size

--- feature-evals/copy_file_range/test.py
+++ feature-evals/copy_file_range/test.py
@@ -3,48 +3,71 @@
 
 import sys
 import os
-import subprocess
+import subprocess # still not adjusted
 import json
 
-def exec(cmd) :
-    fd = subprocess.Popen(cmd, shell=True,\
-            stdin=subprocess.PIPE,\
-            stdout=subprocess.PIPE,\
-            stderr=subprocess.PIPE)
-    return fd.stdout, fd.stderr
-
 # Generate random file for given size
+'''
 if len(sys.argv) != 2 or int (sys.argv[1]) % 512 :
     print ("Usage: test.py <size>, size should be a multiple of 512")
     exit(1)
-
+'''
 #stdout, stderr = exec(git rev-parse --show-toplevel)
-gitroot = str(os.system("git rev-parse --show-toplevel")) + "/"
-print(gitroot)
-if not os.path.isfile ("copy_file_range") :
-    os.system("make copy_file_range")
-size = int (sys.argv[1])
-count = int (size / 512)
+#TODO get path
+#gitroot = str(os.system("git rev-parse --show-toplevel")) + "/"
+#print(gitroot)
+if not os.path.isfile ("copy_file_range_time") or not os.path.isfile ("cp_time"):
+    os.system("make copy_file_range_time cp_time")
+size = 1073741824
+count = int (size / 1024)
 
-fn_origin = "origin_" + str (size) + ".txt"
-fn_copy = "copy_" + str (size) + ".txt"
-fn_cfr = "cfr_" + str (size) + ".txt"
-fn_time = gitroot + "logs/time_" + str (size) + ".log"
-fn_result = gitroot + "logs/copy_file_range_" + str (size) + ".log"
+fn_origin = "origin.txt"
+fn_copy = "copy.txt"
+fn_cfr = "cfr.txt"
+fn_time = "time.log"
+fn_result = "copy_file_range_.log"
 
-os.system ("dd if=/dev/urandom of=" + fn_origin + " count=" + str(count) + " 2>/dev/null")
+os.system ("dd if=/dev/zero of=" + fn_origin + " bs=1k count=" + str(count) + " 2>/dev/null")
+
+# System set
+os.system ("sudo sh -c \"/bin/echo 3 > /proc/sys/vm/drop_caches\"")
+os.system ("sudo sh -c \"/bin/echo 90 > /proc/sys/vm/dirty_ratio\"")
 
 test = 11
 for i in range (test) :
+    os.system ("sudo sh -c \"/bin/echo 3 > /proc/sys/vm/drop_caches\"")
+    os.system ("sudo sh -c \"/bin/echo 3 > /proc/sys/vm/drop_caches\"")
+    os.system ("sudo sh -c \"/bin/echo 3 > /proc/sys/vm/drop_caches\"")
+
+    os.system ("echo cp >> " + fn_time + " 2>&1")
+    os.system ("./cp_time " + fn_origin + " " + fn_copy + " >> " + fn_time + " 2>&1")
+    os.system ("diff " + fn_origin + " " + fn_copy)
+    os.system ("rm " + fn_copy)
+
+    os.system ("sudo sh -c \"/bin/echo 3 > /proc/sys/vm/drop_caches\"")
+    os.system ("sudo sh -c \"/bin/echo 3 > /proc/sys/vm/drop_caches\"")
+    os.system ("sudo sh -c \"/bin/echo 3 > /proc/sys/vm/drop_caches\"")
+
+    os.system ("echo copy_file_range >> " + fn_time + " 2>&1")
+    os.system ("./copy_file_range_time " + fn_origin + " " + fn_cfr + " >> " + fn_time + " 2>&1")
+    os.system ("diff " + fn_origin + " " + fn_cfr)
+    os.system ("rm " + fn_cfr)
+
+    if i == 0 :
+        print ("CPU warming up!")
+    else :
+        print ("Testing copy " + str(size / 1024 / 1024 / 1024) + "GB file (%d/10)" % i )
+'''
     os.system ("( /usr/bin/time -f \"%C\t%U\t%S\t%P\t%e\" cp " + fn_origin + " " + fn_copy + " ) 2>>" + fn_time)
     os.system ("( /usr/bin/time -f \"%C\t%U\t%S\t%P\t%e\" ./copy_file_range " + fn_origin + " " + fn_cfr + " ) 2>>" + fn_time)
     os.system ("rm " + fn_copy)
     os.system ("rm " + fn_cfr)
 
 os.system ("rm " + fn_origin)
-
+'''
 f_time = open (fn_time)
 
+'''
 user_cp = 0.0
 sys_cp = 0.0
 cpu_cp = 0.0
@@ -55,12 +78,9 @@ sys_cfr = 0.0
 cpu_cfr = 0.0
 total_cfr = 0.0
 
-line = f_time.readline ()
-while line :
-    line = line.replace ("\n","")
-    result = line.split ()
-    line = f_time.readline ()
-    if (result[0] == "cp") :
+
+os.system ("sudo sh -c \"/bin/echo 3 > /proc/sys/vm/drop_caches\"")
+   if (result[0] == "cp") :
         user_cp += float (result[3])
         sys_cp += float (result[4])
         result[5] = result[5].replace("%","")
@@ -74,9 +94,11 @@ while line :
         if result[5] != "?" :
             cpu_cfr += float (result[5])
         total_cfr += float (result[6])
+'''
 
 f_time.close ()
 
+'''
 user_cp /= test
 sys_cp /= test
 cpu_cp /= test
@@ -91,4 +113,6 @@ f_result = open (fn_result, "w")
 f_result.write ("\tuser\tsys\tcpu\ttotal\n")
 f_result.write ("cp\t%.2fs\t%.2fs\t%d%%\t%.3f\n" % (user_cp, sys_cp, cpu_cp, total_cp))
 f_result.write ("./copy_file_range\t%.2fs\t%.2fs\t%d%%\t%.3f\n" % (user_cfr, sys_cfr, cpu_cfr, total_cfr))
-
+'''
+f_result = open (fn_result, "w")
+f_result.close ()
