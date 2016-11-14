3369b7c850dd97d9c6f2b4ea5488bd6398073ca0
flavono123 <flavono123@gmail.com>
2016-11-14 16:59:52 +0900

    modified: block comments on 'test.py' are deleted.
    Useless block comments on 'test.py' are removed
    There are some codes not used but commented on 'test.py'. Since the rule that script cannot make any files, include log file, removes that block comments. And by the method 'Popen()' of the module 'subprocess', more flexible result(stdout) control could be possible. This would be added on next commits.

--- feature-evals/copy_file_range/test.py
+++ feature-evals/copy_file_range/test.py
@@ -7,15 +7,6 @@ import subprocess # still not adjusted
 import json
 
 # Generate random file for given size
-'''
-if len(sys.argv) != 2 or int (sys.argv[1]) % 512 :
-    print ("Usage: test.py <size>, size should be a multiple of 512")
-    exit(1)
-'''
-#stdout, stderr = exec(git rev-parse --show-toplevel)
-#TODO get path
-#gitroot = str(os.system("git rev-parse --show-toplevel")) + "/"
-#print(gitroot)
 if not os.path.isfile ("copy_file_range_time") or not os.path.isfile ("cp_time"):
     os.system("make copy_file_range_time cp_time")
 size = 1073741824
@@ -57,62 +48,6 @@ for i in range (test) :
         print ("CPU warming up!")
     else :
         print ("Testing copy " + str(size / 1024 / 1024 / 1024) + "GB file (%d/10)" % i )
-'''
-    os.system ("( /usr/bin/time -f \"%C\t%U\t%S\t%P\t%e\" cp " + fn_origin + " " + fn_copy + " ) 2>>" + fn_time)
-    os.system ("( /usr/bin/time -f \"%C\t%U\t%S\t%P\t%e\" ./copy_file_range " + fn_origin + " " + fn_cfr + " ) 2>>" + fn_time)
-    os.system ("rm " + fn_copy)
-    os.system ("rm " + fn_cfr)
-
-os.system ("rm " + fn_origin)
-'''
 f_time = open (fn_time)
-
-'''
-user_cp = 0.0
-sys_cp = 0.0
-cpu_cp = 0.0
-total_cp = 0.0
-
-user_cfr = 0.0
-sys_cfr = 0.0
-cpu_cfr = 0.0
-total_cfr = 0.0
-
-
-os.system ("sudo sh -c \"/bin/echo 3 > /proc/sys/vm/drop_caches\"")
-   if (result[0] == "cp") :
-        user_cp += float (result[3])
-        sys_cp += float (result[4])
-        result[5] = result[5].replace("%","")
-        if result[5] != "?" :
-            cpu_cp += float (result[5])
-        total_cp += float (result[6])
-    elif (result[0] == "./copy_file_range") :
-        user_cfr += float (result[3])
-        sys_cfr += float (result[4])
-        result[5] = result[5].replace("%","")
-        if result[5] != "?" :
-            cpu_cfr += float (result[5])
-        total_cfr += float (result[6])
-'''
-
 f_time.close ()
 
-'''
-user_cp /= test
-sys_cp /= test
-cpu_cp /= test
-total_cp /= test
-
-user_cfr /= test
-sys_cfr /= test
-cpu_cfr /= test
-total_cfr /= test
-
-f_result = open (fn_result, "w")
-f_result.write ("\tuser\tsys\tcpu\ttotal\n")
-f_result.write ("cp\t%.2fs\t%.2fs\t%d%%\t%.3f\n" % (user_cp, sys_cp, cpu_cp, total_cp))
-f_result.write ("./copy_file_range\t%.2fs\t%.2fs\t%d%%\t%.3f\n" % (user_cfr, sys_cfr, cpu_cfr, total_cfr))
-'''
-f_result = open (fn_result, "w")
-f_result.close ()
