da3f196f5cf53e7892cb4f9d664532fb3b1887e0
Hansuk Hong <flavono123@gmail.com>
2016-11-15 17:29:37 +0900

    copy_file_range: discard output of compile and increase copy file size.
    In before commit, output of compile the binaries by make is printed on log file.
    The useless result is discarded.
    
    And for dramatic difference in test, copying file size is increased.
    But the limited memory of guest machine limits it.
    Consider default memory usage, we copy 3GB file on 4GB memory system.

--- feature-evals/copy_file_range/test_cfr.py
+++ feature-evals/copy_file_range/test_cfr.py
@@ -33,8 +33,8 @@ def copy_test(test):
 
 # Generate random file for given size
 if not os.path.isfile ("copy_file_range_time"):
-    os.system("make copy_file_range_time")
-size = 1073741824
+    os.system("make copy_file_range_time 2>/dev/null") # Meaningless output go to trash
+size = 1073741824 * 3 # Guest Env. 4GB memory
 count = int (size / 1024)
 
 fn_origin = "origin.txt"
